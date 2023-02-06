57c26580cba90fbb7a8b23feff98e98f95562436
Win Feigle <win.feigle@gmail.com>
2023-02-06 09:47:45 -0700

    Complete lab

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { todoAdded } from "./todosSlice";
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  const [ formData, setFormData ] = useState({
+    text: ""
+  })
+  const dispatch = useDispatch();
+
+  const onFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(todoAdded(formData))
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        >
+        <p>
+          <label>add todo</label>
+          <input 
+            type="text" 
+            value={formData.todo}
+            name="text"
+            onChange={onFormChange}
+            />
+        </p>
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default CreateTodo;
